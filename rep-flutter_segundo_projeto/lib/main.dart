047a6c300e011452182023e24c9de4c1f9acb090
Isaias Félix Machado de Alcantara <isaiasofelix@gmail.com>
2023-05-10 09:20:02 -0400

    cvbvcbcb

---
+++ rep-flutter_segundo_projeto/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:segundo_projeto/HomeScreen.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen(),
+  ));
+}
